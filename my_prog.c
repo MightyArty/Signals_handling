5c92468d078c1c635e4b16052264b32c24749f53
MightyArty <tomshabalin95@gmail.com>
2022-03-22 16:23:18 +0000

    some improvments

--- my_prog.c
+++ my_prog.c
@@ -4,27 +4,27 @@
 #include <signal.h>
 
 void handler1(int sig_num){
-    printf("Inside first handler\n");
+    printf("\n Inside first handler\n");
     my_func(sig_num);
 }
 
 void handler2(int sig_num){
-    printf("Inside second handler\n");
+    printf("\n Inside second handler\n");
     my_func(sig_num);
 }
 
 void handler3(int sig_num){
-    printf("Inside third handler\n");
+    printf("\n Inside third handler\n");
     my_func(sig_num);
 }
 
 void handler4(int sig_num){
-    printf("Inside fourth handler\n");
+    printf("\n Inside fourth handler\n");
     my_func(sig_num);
 }
 
 void handler5(int sig_num){
-    printf("Inside fifth handler\n");
+    printf("\n Inside fifth handler\n");
     my_func(sig_num);
 }
 
@@ -32,39 +32,43 @@ void my_func(int S){
     if(S == handler1){
         signal(SIGQUIT,handler2);
         signal(SIGUSR1, handler3);
-        signal(SIGUSR2, handler4);
+        signal(SIGTSTP, handler4);
         signal(SIGHUP, handler5);
     }
     else if(S == handler2){
         signal(SIGQUIT,handler1);
         signal(SIGUSR1, handler3);
-        signal(SIGUSR2, handler4);
+        signal(SIGTSTP, handler4);
         signal(SIGHUP, handler5);
     }
     else if(S == handler3){
         signal(SIGQUIT,handler2);
         signal(SIGUSR1, handler1);
-        signal(SIGUSR2, handler4);
+        signal(SIGTSTP, handler4);
         signal(SIGHUP, handler5);
     }
     else if(S == handler4){
         signal(SIGQUIT,handler2);
         signal(SIGUSR1, handler3);
-        signal(SIGUSR2, handler1);
+        signal(SIGTSTP, handler1);
         signal(SIGHUP, handler5);
     }
     else{
         signal(SIGQUIT,handler2);
         signal(SIGUSR1, handler3);
-        signal(SIGUSR2, handler4);
+        signal(SIGTSTP, handler4);
         signal(SIGHUP, handler1);
     }
 }
 
 int main(){
     printf("Inside main function\n");
+    int i = 0;
     while(1){
         signal(SIGINT, handler1);
+        sleep(1);
+        i++;
+        printf("%d : Wasting time..\n", i);
     }
     return 0;
 }
